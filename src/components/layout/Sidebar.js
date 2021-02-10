6d8714196cef8236ee9986ff5dcbeca5b506ea9d
Abhishek Ranjan <rabhishekranjan@gmail.com>
2021-02-10 11:42:38 +0530

    enabled composite indexing in firebase

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar} from 'react-icons/fa'
 import { useSelectedProjectValue } from '../../context'
+import { Projects } from '../Projects'
 
 export const Sidebar = () => {
-    
+
     const { setSelectedProject } = useSelectedProjectValue
+    const [active, setActive] = useState('inbox')
+    const [showProjects, setShowProjects] = useState(true)
 
     return (
        <div className="sidebar" data-testid="sidebar">
@@ -46,8 +49,8 @@ export const Sidebar = () => {
                <span><FaChevronDown/></span>
                <h2>Projects</h2>
            </div>
-           <ul className="sidebar__projects">Projects will be here!</ul>
-           Add Project Component Here!!
+           <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+           {showProjects && <Projects/>}
        </div>
     )
 }
