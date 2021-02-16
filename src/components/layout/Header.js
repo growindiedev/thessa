249344f0f40f8d2e9360e203687e672600b3eefd
Abhishek Ranjan <rabhishekranjan@gmail.com>
2021-02-16 14:39:57 +0530

    added authentication to header, created login.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,28 @@
 
 import React, { useState } from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
+import {GoSignOut} from 'react-icons/go'
 import { AddTask } from '../AddTask';
+import { useAuth } from "../../context"
+import { Link, useHistory } from "react-router-dom"
+
 
 export const Header = ({darkMode, setDarkMode}) => {
   const [shouldShowMain, setShouldShowMain] = useState(false);
   const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push("/login")
+    } catch {
+      console.log('failed to logout')
+    }
+  }
 
   return (
     <header className="header" data-testid="header">
@@ -32,8 +49,8 @@ export const Header = ({darkMode, setDarkMode}) => {
             >
               <FaPizzaSlice />
             </li>
-            <li className="user__status">
-              logout
+            <li className="user__status" onClick={handleLogout}>
+              <GoSignOut/>
             </li>
           </ul>
         </div>
